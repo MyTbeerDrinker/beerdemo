069feede7ff86bbfc4cbd70749143e703a0b90df
Adam Wonak <adam@getfoodgenius.com>
2014-12-21 20:11:07 -0600

    removed boilerplate controllers and templates. added beer controller and template.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,12 +15,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+        templateUrl: 'views/beer.html',
+        controller: 'BeerCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/beer.js
+++ app/scripts/controllers/beer.js
@@ -8,10 +8,10 @@
  * Controller of the beerdemoApp
  */
 angular.module('beerdemoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('BeerCtrl', function ($scope) {
+    $scope.beers = [
+      'Big Hugs',
+      'Zombie Dust',
+      'Bourbon County Stout'
     ];
   });
